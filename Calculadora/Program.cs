631965acc91c2ed9ae7ba9962c67e9d5482c5a13
SimoneSalvi <simonebonemer@yahoo.com.br>
2023-03-05 10:32:11 -0300

    Implementacao das funcoes (menu, escolha da operacao, 4 operacoes matematicas)

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -8,7 +8,20 @@ internal class Program
         double num1, num2;
         int opcao = 0;
 
-        void Menu()
+        Console.WriteLine("Informe o primeiro numero");
+        num1 = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Informe o segundo numero");
+        num2 = double.Parse(Console.ReadLine());
+
+        while (opcao != 5)
+        {
+            Menu();
+            EscolhaOperacao();
+
+        }
+
+        int Menu()
         {
             Console.Clear();
             Console.WriteLine("1 - Somar");
@@ -17,9 +30,13 @@ internal class Program
             Console.WriteLine("4 - Dividir");
             Console.WriteLine("5 - Sair");
             Console.WriteLine("Escolha uma opção");
-            opcao = int.Parse(Console.ReadLine());
-            
+            opcao =  int.Parse(Console.ReadLine());
+            return opcao;
+ 
+        }
 
+        void EscolhaOperacao()
+        {
             switch (opcao)
             {
                 default:
@@ -27,41 +44,63 @@ internal class Program
                     Console.WriteLine("Escolha uma opção");
                     break;
 
-                  case 1: Console.WriteLine("A soma dos numeros é: " + (num1 + num2));
+                case 1:
+                    soma();
                     Console.ReadLine();
                     break;
 
-                  case 2: Console.WriteLine("A subtração dos numeros é: " + (num1 - num2));
+                case 2:
+                    subtracao();
                     Console.ReadLine();
                     break;
 
                 case 3:
-                    Console.WriteLine("A multiplicação dos numeros é: " + (num1 * num2));
+                    multiplicacao();
                     Console.ReadLine();
                     break;
 
                 case 4:
-                    Console.WriteLine("A divisão dos numeros é: " + (num1/num2));
+                    divisao();
                     Console.ReadLine();
+                    
                     break;
 
-                case 5: Console.WriteLine("Até logo!");
+                case 5:
+                    Console.WriteLine("Até logo!");
                     Console.ReadLine();
                     break;
 
 
             }
         }
+ 
 
-        Console.WriteLine("Informe o primeiro numero");
-        num1 = double.Parse(Console.ReadLine());
+        void soma()
+        {       
+            Console.WriteLine("A soma dos numeros é: " + (num1 + num2));
+        }
 
-        Console.WriteLine("Informe o segundo numero");
-        num2 = double.Parse(Console.ReadLine());
-
-        while(opcao != 5)
+        void subtracao()
         {
-            Menu();
+            Console.WriteLine("A subtracao dos numeros é: " + (num1 - num2));
+        }
+
+        void multiplicacao()
+        {
+            Console.WriteLine("A multiplicacao dos numeros é: " + (num1 * num2));
+        }
+
+        void divisao()
+        {
+            if(num2 != 0)
+            {
+                Console.WriteLine("A divisao dos numeros é: " + (num1 / num2));
+            }
+            else
+            {
+                Console.WriteLine("Não existe divisao por zero.");
+            }
+
         }
 
     }
